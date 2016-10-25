75fdedab354c6762b83a3f824c00bcbc9e8c280b
mohash1311 <moh_ash1311@hotmail.com>
2016-10-25 13:08:18 +0200

    hello_user created

---
+++ HelloWorld/src/HelloUser.java
@@ -0,0 +1,11 @@
+
+public class HelloUser {
+public HelloUser(String username) {
+		super();
+		this.username = username;
+	}
+String username;
+public void greetUser(){
+	System.out.println("Hello "+username+"!");
+}
+}
--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * 
  */
@@ -12,7 +14,14 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		String name;
 		System.out.println("Hello ASE2016 - how are you? :)");
+		System.out.print("Please enter your name: ");
+		Scanner sc = new Scanner(System.in);
+		name = sc.next();
+		HelloUser user = new HelloUser(name);
+		user.greetUser();
+		sc.close();
 	}
 
 }
